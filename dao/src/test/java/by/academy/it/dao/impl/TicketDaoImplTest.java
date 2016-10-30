b546cd26414d29d6493f341d1548687ea0a6948c
andreimozgo <andrei.mozgo@gmail.com>
2016-10-31 00:54:34 +0300

    added junit and refactored command

---
+++ dao/src/test/java/by/academy/it/dao/impl/TicketDaoImplTest.java
@@ -0,0 +1,33 @@
+package by.academy.it.dao.impl;
+
+import by.academy.it.entity.Ticket;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicketDaoImplTest {
+    private Ticket expected;
+
+    @Before
+    public void setUp() {
+        expected = TicketDaoImpl.getInstance().findEntityById(1);
+    }
+
+    @After
+    public void tearDown() {
+        expected = null;
+    }
+
+    @Test
+    public void testTicketDaoImpl() throws Exception {
+        TicketDaoImpl instance1 = TicketDaoImpl.getInstance();
+        TicketDaoImpl instance2 = TicketDaoImpl.getInstance();
+        assertEquals(instance1.hashCode(), instance2.hashCode());
+        expected = TicketDaoImpl.getInstance().findEntityById(1);
+        assertEquals(1, expected.getFligthId());
+    }
+
+
+}
