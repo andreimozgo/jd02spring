acab03a0ed46c84a09181909f43495832c5b1477
andreimozgo <andrei.mozgo@gmail.com>
2016-12-13 22:24:57 +0300

    trying to run tests

--- dao/src/test/java/by/academy/it/dao/impl/FlightDaoImplTest.java
+++ dao/src/test/java/by/academy/it/dao/impl/FlightDaoImplTest.java
@@ -2,6 +2,7 @@ package by.academy.it.dao.impl;
 
 import by.academy.it.dao.exceptions.DaoException;
 import by.academy.it.entity.Flight;
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -17,49 +19,70 @@ import java.util.List;
 @ContextConfiguration("/test-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class})
-@Transactional
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class FlightDaoImplTest extends Assert {
     @Autowired
     FlightDaoImpl flightDao;
+    final Logger LOG = Logger.getLogger(ExtraDaoImpl.class);
 
     @Test
-    public void testCreate() throws DaoException {
-        Flight flight = new Flight(1, "2016-01-01", 20, 10, (byte) 1);
-        flightDao.create(flight);
-        Flight readFlight = flightDao.findEntityById(1);
-        assertNotNull(readFlight);
-        assertEquals(flight, readFlight);
-        flightDao.delete(1);
+    public void testCreate() {
+        try {
+            Flight flight = new Flight(1, "2016-01-01", 20, 10, (byte) 1);
+            flightDao.create(flight);
+            Flight readFlight = flightDao.findEntityById(1);
+            assertNotNull(readFlight);
+            assertEquals(flight, readFlight);
+            flightDao.delete(1);
+        } catch (DaoException e) {
+            LOG.error(e);
+        }
     }
 
     @Test
-    public void testDelete() throws DaoException {
-        Flight flight = new Flight(2, "2016-01-01", 20, 10, (byte) 1);
-        flightDao.create(flight);
-        flightDao.delete(2);
-        Flight deletedFlight = flightDao.findEntityById(2);
-        assertNull(deletedFlight);
+    public void testDelete(){
+        try {
+            Flight flight = new Flight(2, "2016-01-01", 20, 10, (byte) 1);
+            flightDao.create(flight);
+            flightDao.delete(2);
+            Flight deletedFlight = flightDao.findEntityById(2);
+            assertNull(deletedFlight);
+        } catch (DaoException e) {
+            LOG.error(e);
+        }
     }
 
     @Test
-    public void testFindEntityById() throws DaoException {
-        Flight flight = new Flight(3, "2016-11-22", 30, 10, (byte) 0);
-        flightDao.create(flight);
-        Flight readflight = flightDao.findEntityById(3);
-        assertNotNull(readflight);
-        assertEquals(flight, readflight);
-        flightDao.delete(3);
+    public void testFindEntityById(){
+        try {
+            Flight flight = new Flight(3, "2016-11-22", 30, 10, (byte) 0);
+            flightDao.create(flight);
+            Flight readflight = flightDao.findEntityById(3);
+            assertNotNull(readflight);
+            assertEquals(flight, readflight);
+            flightDao.delete(3);
+        } catch (DaoException e) {
+            LOG.error(e);
+        }
     }
 
     @Test
-    public void testGetAmount() throws DaoException {
-        long amount = flightDao.getAmount();
-        assertEquals(0, amount);
+    public void testGetAmount(){
+        try {
+            long amount = flightDao.getAmount();
+            assertEquals(0, amount);
+        } catch (DaoException e) {
+            LOG.error(e);
+        }
     }
 
     @Test
-    public void testGetAll() throws DaoException {
-        List<Flight> flights = flightDao.getAll();
-        assertEquals(0, flights.size());
+    public void testGetAll(){
+        try {
+            List<Flight> flights = flightDao.getAll();
+            assertEquals(0, flights.size());
+        } catch (DaoException e) {
+            LOG.error(e);
+        }
     }
 }
